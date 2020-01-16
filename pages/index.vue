13085d3b15bb1c7bb0353be8aa105bff73ca5922
蔡丽纯 <12345.qq.com>
2020-01-16 20:52:32 +0800

    03-实现首页轮播图(Carousel)，使用的是模拟的轮播图数据

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>首页</div>
+  <div class="container">
+    <!-- Carousel 走马灯：实现首页轮播图 -->
+    <el-carousel height="700px" class="carousel">
+      <el-carousel-item v-for="(item,index ) in banners" :key="index">
+        <div class="banner" :style="`background:url(${item}) center center no-repeat`"></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      // 轮播图数据
+      banners:[
+        'http://157.122.54.189:9095/assets/images/th01.jfif',
+        'http://157.122.54.189:9095/assets/images/th02.jfif',
+        'http://157.122.54.189:9095/assets/images/th03.jfif'
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.carousel {
+	min-width: 1000px;
+}
+.banner {
+	height: 700px;
+}
 </style>
