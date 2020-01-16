1a305ef346b100789f0b5ae292281c5836494e16
蔡丽纯 <12345.qq.com>
2020-01-16 21:11:07 +0800

    04-实现搜索框的布局和tab栏的操作

--- pages/index.vue
+++ pages/index.vue
@@ -6,29 +6,178 @@
         <div class="banner" :style="`background:url(${item}) center center no-repeat`"></div>
       </el-carousel-item>
     </el-carousel>
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item, index) in options"
+            :key="index"
+            :class="{active: index === currentOption}"
+            @click="handleOption(index)"
+          >
+            <i>{{item.name}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input
+            :placeholder="options[currentOption].placeholder"
+          />
+          <i class="el-icon-search" @click="handleSearch"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       // 轮播图数据
-      banners:[
-        'http://157.122.54.189:9095/assets/images/th01.jfif',
-        'http://157.122.54.189:9095/assets/images/th02.jfif',
-        'http://157.122.54.189:9095/assets/images/th03.jfif'
-      ]
-    }
+      banners: [
+        "http://157.122.54.189:9095/assets/images/th01.jfif",
+        "http://157.122.54.189:9095/assets/images/th02.jfif",
+        "http://157.122.54.189:9095/assets/images/th03.jfif"
+      ],
+      // 搜索栏的数据
+      options: [
+        {
+          name: "攻略",
+          placeholder: "搜索城市"
+        },
+        {
+          name: "酒店",
+          placeholder: "请输入城市搜索酒店"
+        },
+        {
+          name: "机票",
+          placeholder: ""
+        }
+      ],
+      // 当前高亮的搜索栏按钮
+      currentOption:0
+    };
+  },
+  methods:{
+    // 处理点击搜索栏按钮的事件
+    handleOption(index){
+      // 设置当前被点击的tab栏
+      this.currentOption=index
+      // 如果点击的是机票，则要跳转到机票的首页
+      if(index===2){
+        this.$router.push('/air')
+      }
+    },
+    // 处理点击搜索按钮的事件
+    handleSearch(){}
   }
-}
+};
 </script>
 
 <style lang='less' scoped>
 .carousel {
-	min-width: 1000px;
+  min-width: 1000px;
 }
 .banner {
-	height: 700px;
+  height: 700px;
+}
+.banner-image {
+  width: 100%;
+  height: 100%;
+}
+
+.banner-content {
+  z-index: 9;
+  width: 1000px;
+  position: absolute;
+  left: 50%;
+  top: 45%;
+  margin-left: -500px;
+  border-top: 1px transparent solid;
+
+  .search-bar {
+    width: 552px;
+    margin: 0 auto;
+  }
+
+  .search-tab {
+    .active {
+      i {
+        color: #333;
+      }
+      &::after {
+        background: #eee;
+      }
+    }
+
+    span {
+      width: 82px;
+      height: 36px;
+      display: block;
+      position: relative;
+      margin-right: 8px;
+      cursor: pointer;
+
+      i {
+        position: absolute;
+        z-index: 2;
+        display: block;
+        width: 100%;
+        height: 100%;
+        line-height: 30px;
+        text-align: center;
+        color: #fff;
+        font-style:normal;
+
+      }
+
+      &:after {
+        position: absolute;
+        left: 0;
+        top: 0;
+        display: block;
+        content: "";
+        width: 100%;
+        height: 100%;
+        border: 1px rgba(255, 255, 255, 0.2) solid;
+        border-bottom: none;
+        transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+        transform-origin: bottom left;
+        background: rgba(0, 0, 0, 0.5);
+        border-radius: 1px 2px 0 0;
+        box-sizing: border-box;
+      }
+    }
+  }
+
+  .search-input {
+    width: 550px;
+    height: 46px;
+    background: #fff;
+    border-radius: 0 4px 4px 4px;
+    border: 1px rgba(255, 255, 255, 0.2) solid;
+    border-top: none;
+    box-sizing: unset;
+
+    input {
+      flex: 1;
+      height: 20px;
+      padding: 13px 15px;
+      outline: none;
+      border: 0;
+      font-size: 16px;
+    }
+
+    .el-icon-search {
+      cursor: pointer;
+      font-size: 22px;
+      padding: 0 10px;
+      font-weight: bold;
+    }
+  }
 }
 </style>
