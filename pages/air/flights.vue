7be5dc6207ed71e8e0e772a9f995e29fae1f743b
蔡丽纯 <12345.qq.com>
2020-02-12 22:11:47 +0800

    19-实现分页功能

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -7,10 +7,30 @@
         <div></div>
 
         <!-- 航班头部布局 -->
-        <FlightsListHead/>
+        <FlightsListHead />
 
         <!-- 航班信息 -->
-        <FlightsItem v-for="(item,index) in flightsData.flights" :key="index" :data='item'/>
+        <FlightsItem v-for="(item,index) in pageSizeList" :key="index" :data="item" />
+
+        <!-- 分页组件 -->
+        <!-- 
+            size-change：切换每页显示数量列表时触发
+            current-change：切换页码时触发
+            current-page：当前页码
+            page-sizes：每页显示的数量的列表
+            page-size：每页显示的数量
+            layout：分页的组成部分
+            total：总数
+        -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageIndex"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
       </div>
 
       <!-- 侧边栏 -->
@@ -22,29 +42,61 @@
 </template>
 
 <script>
-import FlightsListHead from '@/components/air/flightsListHead'
-import FlightsItem from '@/components/air/flightsItem'
+import FlightsListHead from "@/components/air/flightsListHead";
+import FlightsItem from "@/components/air/flightsItem";
 export default {
   data() {
     return {
-        // 机票列表的数据
-        flightsData:{}
+      // 机票列表的数据
+      flightsData: {},
+      // 当前页数
+      pageIndex: 1,
+      // 当前的条数
+      pageSize: 5,
+      // 总条数
+      total: 0
     };
   },
-  components:{
-      FlightsListHead,
-      FlightsItem
+  components: {
+    FlightsListHead,
+    FlightsItem
   },
-  mounted(){
+  mounted() {
     //   请求机票列表数据
     this.$axios({
-        url:'/airs',
-        params:this.$route.query
-    }).then(res=>{
-        console.log(res);
-        // 有flights、info、options、total属性
-        this.flightsData=res.data  
-    })
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+      //   console.log(res);
+      // 有flights、info、options、total属性
+      this.flightsData = res.data;
+
+      // 修改总数
+      this.total = this.flightsData.total;
+    });
+  },
+  methods: {
+    // 切换每页显示数量列表时触发
+    handleSizeChange(index) {
+      this.pageSize = index;
+    },
+    // 切换页码时触发
+    handleCurrentChange(index) {
+      this.pageIndex = index;
+    }
+  },
+  computed: {
+    pageSizeList() {
+      // 判断flightsData有没有值
+      if (!this.flightsData.flights) {
+        return [];
+      }
+      const arr = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+      return arr;
+    }
   }
 };
 </script>
